93709172c3345ad9a5482e4846937c9f7e7bb53b
Emon Joy <111835338+EmonJoy@users.noreply.github.com>
2022-09-04 09:01:54 +0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<conio.h>
+#include <stdlib.h>
+using namespace std;
+
+
+int main(){
+    while (true)
+    {
+        int guess, number;
+        cout<<"Enter number between 1 to 20: "<<endl<<endl<<">>>> ";
+        cin>>guess;
+
+        number = rand()%20;
+
+        if (guess == number)
+        {
+            cout<<"You won the game"<<endl<<endl;
+            break;
+        }else
+        {
+            cout<<"Incorrect number... try again!"<<endl<<endl;
+            cout<<"Random number was: "<<number<<endl<<endl;
+        }
+        
+        
+
+    }
+    
+
+
+    getch();
+}
